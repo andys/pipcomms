dcbf45c620e83605212003544609cf6d90a265e0
Andrew Snow <andrew@modulus.org>
2016-12-10 12:52:08 +1100

    add proper buffering and retry

--- pip.c
+++ pip.c
@@ -5,36 +5,35 @@
 #include <fcntl.h>
 #include <termios.h>
 #include <stdint.h>
+#include <sys/time.h>
+#include <sys/types.h>
 
-#define INT16U uint16_t
-#define INT8U uint8_t
-
-INT16U cal_crc_half(INT8U *pin, INT8U len)
+uint16_t cal_crc_half(uint8_t *pin, uint8_t len)
 {
-  INT16U crc;
-  INT8U da;
-  INT8U *ptr;
-  INT8U bCRCHign;
-  INT8U bCRCLow;
-  INT16U crc_ta[16]=
-  { 
+  uint16_t crc;
+  uint8_t da;
+  uint8_t *ptr;
+  uint8_t bCRCHign;
+  uint8_t bCRCLow;
+  uint16_t crc_ta[16]=
+  {
     0x0000,0x1021,0x2042,0x3063,0x4084,0x50a5,0x60c6,0x70e7,
     0x8108,0x9129,0xa14a,0xb16b,0xc18c,0xd1ad,0xe1ce,0xf1ef
   };
   ptr=pin;
   crc=0;
-  while(len--!=0) 
+  while(len--!=0)
   {
-    da=((INT8U)(crc>>8))>>4; 
+    da=((uint8_t)(crc>>8))>>4;
     crc<<=4;
-    crc^=crc_ta[da^(*ptr>>4)]; 
-    da=((INT8U)(crc>>8))>>4; 
+    crc^=crc_ta[da^(*ptr>>4)];
+    da=((uint8_t)(crc>>8))>>4;
     crc<<=4;
-    crc^=crc_ta[da^(*ptr&0x0f)]; 
+    crc^=crc_ta[da^(*ptr&0x0f)];
     ptr++;
   }
   bCRCLow = crc;
-  bCRCHign= (INT8U)(crc>>8);
+  bCRCHign= (uint8_t)(crc>>8);
   if(bCRCLow==0x28||bCRCLow==0x0d||bCRCLow==0x0a)
   {
        bCRCLow++;
@@ -43,7 +42,7 @@ INT16U cal_crc_half(INT8U *pin, INT8U len)
   {
         bCRCHign++;
   }
-  crc = ((INT16U)bCRCHign)<<8;
+  crc = ((uint16_t)bCRCHign)<<8;
   crc += bCRCLow;
   return(crc);
 }
@@ -51,13 +50,16 @@ INT16U cal_crc_half(INT8U *pin, INT8U len)
 
 int main(int argc, char *argv[])
 {
-  int fd, result, len;
-  INT16U crc;
+  int fd, result, len, retries;
+  uint16_t crc;
   char port[] = "/dev/ttyUSB0";
   unsigned char inbuf[256], outbuf[256];
   unsigned char *ch, cr = '\n';
   speed_t baud = B2400;
-  fd = open(port, O_RDWR);
+  /*fd_set listen_fds;
+  struct timeval timeout;*/
+
+  fd = open(port, O_RDWR | O_NONBLOCK);
 
   struct termios settings;
   tcgetattr(fd, &settings);
@@ -75,7 +77,7 @@ int main(int argc, char *argv[])
 
   while(fgets(inbuf, 256 , stdin) != NULL) {
     inbuf[strcspn(inbuf, "\n")] = 0;
-    crc = cal_crc_half((INT8U *) inbuf, (INT8U)strlen(inbuf));
+    crc = cal_crc_half((uint8_t *) inbuf, (uint8_t)strlen(inbuf));
     sprintf(outbuf, "%s%c%c\r", inbuf, 0xff & (crc >> 8), 0xff & crc);
     len = strlen(inbuf) + 3;
 
@@ -94,20 +96,30 @@ int main(int argc, char *argv[])
     ch = inbuf;
     *ch = 0;
 
-    for(;;) {
-      result = read(fd, ch, 1);
-      if(result != 1) {
-         printf("failed to read (1)\n");
-         exit(1);
+    for(retries = 0; 1; retries++) {
+      usleep(100000);
+      result = read(fd, inbuf, 255);
+      if(result < 1) {
+        if(retries >= 10) {
+          if (write(fd, "\r", 1) != 1) {
+            printf("failed to write (1)\n");
+            exit(1);
+          }
+          retries = 0;
+        }
+      } else {
+        ch = inbuf + result - 1;
+        if(*ch == '\r' || *ch == '\n') {
+          ch -= 2;
+          if(ch >= inbuf) {
+            *ch = 0;
+            break;
+          }
+        }
       }
-      if(*ch == '\r' || *ch == '\n') {
-        ch -= 2;
-        *ch = 0;
-        break;
-      }
-      ch++;
     }
     printf("%s\n", inbuf+1);
+    fflush(stdout);
   }
   return 0;
 }
